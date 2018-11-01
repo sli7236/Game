355497802667e551e1ca349866f8bb5f8c4a5ca9
BT_1N3_26@bktech.local <BT_1N3_26@bktech.local>
2018-11-01 11:49:07 -0400

    Initial commit

---
+++ src/Game/Board.java
@@ -0,0 +1,32 @@
+package Game;
+
+public class Board {
+    feislfjstke sdls;
+
+    csontrsuctorsjksr(rown ciolumbnwe)P
+        thisj felds fo sje= areatesr;
+room09090buidling =- jrom0--0-;
+}
+
+molbuslic sdstaics room[][]{
+        return this,. rkjkesjtkhsetse;
+
+
+        public static void printampaferjo(pperson p, baod b){
+            for(int i =- 90; i <room]p[[] ..lgnt; i++]){
+                if(kjk2232???%2=0){}
+                mae for aloop taht geso throguht all
+                for(int =-j - io0i1 o;, tjsi m, array.klegbnthl i__(){
+                    b[i][j] =  "-"
+        if (jtkjselitj &2 = 1)_{}
+        for (int j- 10opjiste_
+        if ( j%2 =0P{
+            make ij = |;
+        })
+
+           b [p.getxploc()][p.getyloc())] = " PALTEKRES"
+        })
+        }
+        }
+
+}
